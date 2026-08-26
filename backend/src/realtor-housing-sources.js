71e01e59e8d757dfd5b7adac65fb3e9438920bfc
MARHARYTA KUBAI <44707323+AmoneMisa@users.noreply.github.com>
2026-08-27 01:43:11 +0300

    feat: add curated realtor housing sources

---
+++ backend/src/realtor-housing-sources.js
@@ -0,0 +1,45 @@
+export const REALTOR_HOUSING_SOURCES = {
+  UZ: [
+    {
+      key: 'hata-tashkent-rent',
+      url: 'https://www.hata.uz/listings/rent/tashkent',
+      city: 'Tashkent',
+    },
+    {
+      key: 'realting-tashkent-rent',
+      url: 'https://realting.uz/tashkent/property-to-rent/apartments',
+      city: 'Tashkent',
+    },
+    {
+      key: 'rentli-tashkent-rent',
+      url: 'https://rentli.uz/ru',
+      city: 'Tashkent',
+    },
+  ],
+  UA: [
+    {
+      key: 'x-estate-ukraine-rent',
+      url: 'https://www.x-estate.com/orenduvaty-kvartyru',
+      city: null,
+    },
+    {
+      key: 'park-lane-kyiv-rent',
+      url: 'https://parklane.ua/uk/realty_search/apartment/rent',
+      city: 'Kyiv',
+    },
+    {
+      key: 'blagovist-kyiv-rent',
+      url: 'https://blagovist.ua/search/apartment/rent',
+      city: 'Kyiv',
+    },
+    {
+      key: 'atlanta-odesa-rent',
+      url: 'https://www.atlanta.ua/uk/odessa/filters/arenda/kvartiry',
+      city: 'Odesa',
+    },
+  ],
+};
+
+export function realtorHousingSources(countryCode) {
+  return REALTOR_HOUSING_SOURCES[String(countryCode || '').toUpperCase()] || [];
+}
